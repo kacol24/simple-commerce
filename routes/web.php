e0515b7bfe72dbc881533478f5a391eab659a122
Kevin Chandra <kacol.bot@gmail.com>
2024-05-01 02:08:20 +0700

    cleanup sandbox welcome

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $order = Order::first();
-    dd($order->status);
-
     return view('welcome');
 });
