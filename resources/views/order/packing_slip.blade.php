2410956ba5c0bee7b9a836fedd6cf1d4c5fa2433
Kevin Chandra <kacol.bot@gmail.com>
2024-05-09 22:10:00 +0700

    fontawesome

--- resources/views/order/packing_slip.blade.php
+++ resources/views/order/packing_slip.blade.php
@@ -6,8 +6,7 @@
     <title>Print Packing Slip | {{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css">
     <style>
         @page {
             size: 58mm 200mm;
